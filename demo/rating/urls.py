3e34ec5a02746108ddff1a946cc743c38fb50095
Zhiwei <sc16zr@leeds.ac.uk>
2020-03-06 16:25:20 +0800

    registration demo

---
+++ demo/rating/urls.py
@@ -0,0 +1,8 @@
+from rating import views
+from django.urls import path, re_path
+
+urlpatterns = [
+    path('users/', views.UsersAPIView.as_view()),
+    re_path('users/(?P<pk>\d+)/', views.UserAPIView.as_view(),name='user-detail'),
+
+]
--- demo/rating/views.py
+++ demo/rating/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rating.models import User
+from rating.serializers import UserSerializer
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+class UsersAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserAPIView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
