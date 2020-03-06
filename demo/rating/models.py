5320fb09b306186dc5f5e747c7ce0607f9368097
Zhiwei <sc16zr@leeds.ac.uk>
2020-03-06 14:54:24 +0800

    year and semester

--- demo/rating/models.py
+++ demo/rating/models.py
@@ -4,23 +4,31 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class User(models.Model):
-    username = models.CharField(max_length = 16,unique = True)
-    password = models.CharField(max_length = 16)
-    u_id = models.IntegerField(primary_key = True)
+    username = models.CharField(max_length=16, unique=True)
+    password = models.CharField(max_length=16)
+    u_id = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.username
 
+
 class Professor(models.Model):
-    firstname = models.CharField(max_length = 10)
-    lastname = models.CharField(max_length = 10)
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=10)
     prof_id = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.firstname
 
+
 class Module(models.Model):
     module_name = models.CharField(max_length=20)
+    year = models.IntegerField(default=2000)
+    semester = models.IntegerField(default=1,
+                                   validators=[
+                                       MaxValueValidator(2),
+                                       MinValueValidator(1)
+                                   ])
     description = models.TextField()
     module_id = models.IntegerField(primary_key=True)
     prof = models.ManyToManyField(Professor)
@@ -28,8 +36,9 @@ class Module(models.Model):
     def __str__(self):
         return self.module_name
 
+
 class Score(models.Model):
-    score_id = models.IntegerField(primary_key = True)
+    score_id = models.IntegerField(primary_key=True)
     score = models.IntegerField(
         default=1,
         validators=[
@@ -37,8 +46,8 @@ class Score(models.Model):
             MinValueValidator(1)
         ]
     )
-    professor = models.ForeignKey(Professor, on_delete = models.CASCADE)
-    module = models.ForeignKey(Module, on_delete = models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "Module: " + self.module.module_name + " Prof: " +self.professor.lastname + " Score: " + str(self.score)
+        return "Module: " + self.module.module_name + " Prof: " + self.professor.lastname + " Score: " + str(self.score)
