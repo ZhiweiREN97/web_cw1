03275e0073d5f123c6d58147c5d1eaf22bfab0a2
Zhiwei <sc16zr@leeds.ac.uk>
2020-03-13 12:10:04 +0800

    client demo

---
+++ demo/client.py
@@ -0,0 +1,22 @@
+import requests
+
+while(True):
+    val = input("Please enter your command:")
+    val = val.split(" ")
+    if val[0] == 'exit':
+        print ("Thank you for using the rating system!")
+        break
+    elif val[0] == 'register':
+        pass
+    elif val[0] == 'login':
+        pass
+    elif val[0] == 'logout':
+        pass
+    elif val[0] == 'list':
+        pass
+    elif val[0] == 'view':
+        pass
+    elif val[0] == 'average':
+        pass
+    elif val[0] == 'rate':
+        pass
